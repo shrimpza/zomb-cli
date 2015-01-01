b4e715c7b8c3a7465bb425fd1605d9b4bf22e1a6
shrimpza <git@shrimpworks.za.net>
2015-01-01 11:36:07 +0200

    test plan

--- test/net/shrimpworks/zomb/cli/MainTest.java
+++ test/net/shrimpworks/zomb/cli/MainTest.java
@@ -1,5 +1,7 @@
 package net.shrimpworks.zomb.cli;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
@@ -7,7 +9,15 @@ import static org.junit.Assert.fail;
 public class MainTest {
 
 	@Test
-	public void mainTest() {
+	public void mainTest() throws IOException {
+		// TODO construct temporary config file
+
+		// TODO execute Main.main(...)
+
+		// TODO capture output from stdout and validate
+
+		// TODO clean up config file
+
 		fail("Todo");
 	}
 }
